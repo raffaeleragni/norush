342ff08ad9d9b19d66e976f0a9c2bf27a5748fdf
Raffaele Ragni <raffaele.ragni@gmail.com>
2023-12-05 00:55:18 +0100

    setup mock data

--- src/app.rs
+++ src/app.rs
@@ -14,12 +14,25 @@ struct Index;
 #[derive(Template, Default)]
 #[template(path = "board.html")]
 struct Board {
+    states: Vec<StateCards>,
+}
+
+#[derive(Default)]
+struct StateCards {
+    state: &'static str,
     cards: Vec<Card>,
 }
 
 #[derive(Template, Default)]
 #[template(path = "card.html")]
-struct Card;
+struct CardPage {
+    card: Card,
+}
+
+#[derive(Default)]
+struct Card {
+    title: &'static str,
+}
 
 async fn index() -> Index {
     Index {}
@@ -27,10 +40,49 @@ async fn index() -> Index {
 
 async fn board() -> Board {
     Board {
-        cards: vec![Card {}],
+        states: vec![
+            StateCards {
+                state: "todo",
+                cards: vec![
+                    Card { title: "user actions" },
+                    Card { title: "connect to database" },
+                ],
+            },
+            StateCards {
+                state: "in progress",
+                cards: vec![
+                    Card { title: "mock data" },
+                    Card { title: "refactor" },
+                    Card {
+                        title: "setup layout",
+                    },
+                ],
+            },
+            StateCards {
+                state: "done",
+                cards: vec![
+                    Card {
+                        title: "setup project",
+                    },
+                    Card {
+                        title: "setup build tools",
+                    },
+                    Card {
+                        title: "create github",
+                    },
+                    Card {
+                        title: "announce project",
+                    },
+                ],
+            },
+        ],
     }
 }
 
-async fn card() -> Card {
-    Card {}
+async fn card() -> CardPage {
+    CardPage {
+        card: Card {
+            title: "sample title",
+        },
+    }
 }
