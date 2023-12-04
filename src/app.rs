5f7234894c4d9076b68d8dcdc6d3cd551d46ac76
Raffaele Ragni <raffaele.ragni@gmail.com>
2023-12-04 16:10:30 +0100

    setup three templates

--- src/app.rs
+++ src/app.rs
@@ -3,12 +3,34 @@ use axum::{routing::get, Router};
 
 pub fn init(app: Router) -> Router {
     app.route("/", get(index))
+        .route("/board", get(board))
+        .route("/card", get(card))
 }
 
 #[derive(Template)]
 #[template(path = "index.html")]
 struct Index;
 
+#[derive(Template, Default)]
+#[template(path = "board.html")]
+struct Board {
+    cards: Vec<Card>,
+}
+
+#[derive(Template, Default)]
+#[template(path = "card.html")]
+struct Card;
+
 async fn index() -> Index {
     Index {}
+}
+
+async fn board() -> Board {
+    Board {
+        cards: vec![Card {}],
+    }
+}
+
+async fn card() -> Card {
+    Card {}
 }
